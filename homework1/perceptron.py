65908cb24c29d07b5ddb107ac7bac3a71000ee4f
Shubham Gupta <shubham.gupta30@gmail.com>
2014-05-19 19:55:02 +0530

    completed perceptron

--- homework1/perceptron.py
+++ homework1/perceptron.py
@@ -54,7 +54,6 @@ def plot_line(f):
   else:
     point1 = (-1, (-f[0] + f[1])/f[2])
     point2 = (1, (-f[0] - f[1])/f[2])
-  print point1, point2
   (x1, y1) = point1
   (x2, y2) = point2
   pylab.plot([x1,x2], [y1,y2])
@@ -68,11 +67,70 @@ def plot_data(training_set, f):
   Y  = [item[0][2] for item in training_set]
   C  = [item[1] for item in training_set]
   pylab.scatter(X, Y, c=C, s=50, alpha=.5)
-  pylab.show()
 
-def generate_learning_instance(size):
+##########################################################################
+
+# Get a misclassified point
+def get_misclassified_point(training_set, w):
+  for item in training_set:
+    if classify(item[0],w) != item[1]:
+      return item
+
+# Modify w
+def modify(w, (x,y)):
+  return w + y*x
+
+# PLA Algorithm
+def pla(training_set, num_iters=1024):
+  extra_info = []
+  w = random_f()
+  extra_info.append(calculate_classification_error(training_set, w))
+  iters = 0
+  while num_iters > iters:
+    try:
+      (x,y) = get_misclassified_point(training_set, w)
+    except TypeError:
+      print "No more misclassified points after %d iterations" % (iters)
+      return w, extra_info
+    w = modify(w, (x,y))
+    extra_info.append(calculate_classification_error(training_set, w))
+    iters += 1
+  return w, extra_info
+
+##########################################################################
+
+def calculate_classification_error(training_data, w):
+  total = 0.0
+  misclassified = 0.0
+  for item in training_data:
+    total += 1.0
+    if classify(item[0], w) != item[1]:
+      misclassified += 1.0
+  return misclassified/total
+
+def plot_error(extra_info):
+  X = range(0, len(extra_info))
+  pylab.plot(X, extra_info)
+
+def calculate_Eout(f, g, num_iters):
+  misclassified = 0.0
+  for i in xrange(num_iters):
+    (x,y) = random_point()
+    point = array([1, x, y])
+    if classify(point, f) != classify(point, g):
+      misclassified += 1.0
+  return misclassified/num_iters
+
+
+def generate_and_solve_learning_instance(size, algo_iterations = 1024, eout_iters=1024):
   f = random_f()
   training_data = training_set_random(size, f)
   plot_data(training_data, f)
+  w, extra_info = pla(training_data, algo_iterations)
+  plot_line(w)
+  pylab.show()
+  plot_error(extra_info)
+  pylab.show()
+  print "Eout estimate %f" % calculate_Eout(f, w, eout_iters)
   return training_data, f
 
