8fefa3840d22f4448ebae4a4c5bf88b2e8273f99
Shubham Gupta <shubham.gupta30@gmail.com>
2014-05-20 01:41:34 +0530

    completed eout calculation

--- homework 2/linear_regression.py
+++ homework 2/linear_regression.py
@@ -13,7 +13,10 @@ def create_training_set(size=100):
   Y = array([item[1] for item in training_data])
   return X, Y, f
 
-def solve_one_regression(size = 100, plot_solu = True):
+def solve_one_regression(
+    size = 100,
+    plot_solu = True,
+    calculate_eout=False):
   X,Y,f = create_training_set(size)
   w = pinv(X).dot(Y)
   if plot_solu:
@@ -24,6 +27,7 @@ def solve_one_regression(size = 100, plot_solu = True):
     perceptron.plot_line(f)
     perceptron.plot_line(w)
     pylab.show()
+    if calculate_eout: print "eout: %f" % get_eout(f,w)
   return compute_ein(X,f,w)
 
 def compute_ein(X, f, g):
@@ -37,3 +41,15 @@ def repeat(size=100, num_times=1000):
   ein_values = [solve_one_regression(size, False) for i in xrange(num_times)]
   print ein_values
   return average(ein_values)
+
+def get_eout(f, g, num_points=1000, num_iters=1000):
+  eout_sum = 0.0
+  for j in xrange(num_iters):
+    misclassified = 0.0
+    for i in xrange(num_points):
+      point = perceptron.random_point() 
+      point = array([1, point[0], point[1]])
+      misclassified += (
+          perceptron.classify(f, point) - perceptron.classify(g, point))**2/4
+    eout_sum += float(misclassified)/float(num_points)
+  return eout_sum/float(num_iters)
